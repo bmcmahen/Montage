03fa6e33927888659cbf06f6a7153c7a65823ff2
Ben McMahen <ben.mcmahen@gmail.com>
2013-08-15 02:06:25 -0600

    zoom doesnt zoom on reloads

--- public/app.js
+++ public/app.js
@@ -23069,17 +23069,17 @@ var currentZoom;
 
 Session.on('change:imageZoom', zoom);
 var holdover = Session.get('imageZoom');
-if (holdover) zoom(holdover);
+if (holdover) zoom(holdover, { noanimate: true });
 
 $container.on('click', function(e){
 	currentZoom && zoom();
 });
 
-function zoom(val){
+function zoom(val, options){
 	if (currentZoom) currentZoom.hide();
 	if (val) {
-		currentZoom = constructZoom(val.origin, val.url, val.fn);
-		if (val.model){
+		currentZoom = constructZoom(val.origin, val.url, val.fn, options);
+		if (val.model && val.model.on){
 			val.model.on('change:original_backdrop_path', function(model, img){
 				if (img) {
 					$container
@@ -23092,20 +23092,26 @@ function zoom(val){
 	}
 }
 
-function constructZoom(origin, url, fn){
+function constructZoom(origin, url, fn, options){
 	var top = 45;
 	var left = 0;
 	var height = window.innerHeight - 45;
 	var width = window.innerWidth;
 	var originHeight = origin.width * (height / width);
+	options = options || {};
 
 	var zoom = new BackgroundZoom(url, $container.get())
 		.duration(300)
 		.setDimensions('100%', '100%')
 		.className('zoom-background')
 		.target(left, top, width, height)
-		.origin(origin.left, origin.top + 130, origin.width, originHeight)
-		.show();
+		.origin(origin.left, origin.top + 130, origin.width, originHeight);
+
+	if (options.noanimate) {
+		zoom.className('no-zoom');
+	}
+
+	zoom.show();
 
 	if (fn) fn(zoom);
 
